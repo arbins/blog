ada99ca920b2c7befde848f2161c99e1b9307052
arbins <arbins@jobready.com.au>
2017-03-04 17:23:57 +1100

    added devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email, :password, :given_name, :last_name)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:username, :email, :password, :current_password, :password_confirmation, :given_name, :last_name)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :articles
-
-  root 'welcome#index'
+  devise_for :users
+  resources :articles do
+    resources :comments
+  end
+  root to: 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
